db2cd1a7c4371615004bd22adb846f0f81e980bc
Akq00 <94464764+Akq00@users.noreply.github.com>
2022-02-08 16:55:56 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+function myFunction() {
+	document.getElementById('Ls').style.color = "blue";
+}
+
+function myFunctionS() {
+	document.getElementById('LIS').style.color = "green";
+}
+
+function myFunctionN() {
+	document.getElementById('p2').style.color = "green";
+}
+
+function myFunctionn() {
+	document.getElementById('p1').style.color = "green";
+}
+
+
+function MyFnct() {
+
+	document.getElementById('LIS').innerHTML = "LIS" ;
+}
+
+
+function MyFUct() {
+
+	document.getElementById('L').innerHTML = "LIS" ;
+}
+
+
+
